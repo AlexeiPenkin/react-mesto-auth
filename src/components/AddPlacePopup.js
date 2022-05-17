c12f13d81b5b1111bd6ddb89dcf33099999698cd
Alexei Penkin <iva13293376@yandex.ru>
2022-05-17 20:48:55 +0500

    first commit

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PopupWithForm from './PopupWithForm';
 
-
-
 export default function AddPlacePopup(props) {
 
   const [name, setName] = useState('');
